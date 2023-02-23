d78f181ceca7c890c7ba7d8e54f2fb853fb4610f
Fernando Siqueira <fernandosiqueira994@gmail.com>
2023-02-23 01:08:48 -0300

    adequando variáveis de var para let

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var inputTexto = document.querySelector("#entrada-texto");
 var outputTexto = document.querySelector("#saida-texto");
 
 function trocaDiamantePorOutput() {
-    var telaOutputTexto = document.querySelector(".saida-estado-ativo");
-    var telaDiamante = document.querySelector(".saida-estado-espera");
-    var btnCopiar = document.querySelector(".btn-copiar");
+    let telaOutputTexto = document.querySelector(".saida-estado-ativo");
+    let telaDiamante = document.querySelector(".saida-estado-espera");
+    let btnCopiar = document.querySelector(".btn-copiar");
 
     telaOutputTexto.setAttribute('style', '');
     telaDiamante.setAttribute('style', 'display: none');
@@ -14,17 +14,17 @@ function trocaDiamantePorOutput() {
 
 function cifrar(textoBase, boolEncripta) {
 
-    var arrayCifra = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    let arrayCifra = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
     textoBase = textoBase.toLowerCase();
     
     if(boolEncripta){
-        for(var i = 0; i < arrayCifra.length; i++) {
+        for(let i = 0; i < arrayCifra.length; i++) {
             if(textoBase.includes(arrayCifra[i][0])) {
                 textoBase = textoBase.replaceAll(arrayCifra[i][0], arrayCifra[i][1]);
             }
         }
     } else {
-        for(var i = 0; i < arrayCifra.length; i++) {
+        for(let i = 0; i < arrayCifra.length; i++) {
             if(textoBase.includes(arrayCifra[i][1])) {
                 textoBase = textoBase.replaceAll(arrayCifra[i][1], arrayCifra[i][0]);
             }
